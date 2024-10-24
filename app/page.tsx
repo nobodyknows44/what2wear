2f61dbb98df3676e62c0f2362222365642c99c78
Alex <thadarknight@192.168.2.27>
2024-10-24 13:09:57 +0700

    Fix unused import and apostrophe issues

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import WeatherDashboard from './components/WeatherDashboard'
 export default function Home() {
   return (
     <div className="min-h-screen w-full flex items-center justify-center bg-cover bg-center bg-no-repeat" style={{ backgroundImage: "url('/background.jpg')" }}>
-      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="absolute inset-0 bg-black opacity-20"></div>
       <div className="relative z-10 w-full h-full flex items-center justify-center">
         <WeatherDashboard />
       </div>
