02166c9fc8c5ced0aa6b275b1840196008316eae
Alex <thadarknight@Alexs-MacBook-Pro.local>
2024-10-30 16:22:09 +0700

    Hie

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,29 +1,29 @@
 export interface LocationData {
-  city: string
-  country: string
+  city: string;
+  country: string;
 }
 
 export interface WeatherData {
-  temperature: number
-  feelsLike: number
-  conditions: string
-  windSpeed: number
-  humidity: number
+  temperature: number;
+  feelsLike: number;
+  conditions: string;
+  windSpeed: number;
+  humidity: number;
   upcomingHours: Array<{
-    time: string
-    temperature: number
-    conditions: string
-  }>
+    time: string;
+    temperature: number;
+    conditions: string;
+  }>;
 }
 
 export interface ClothingItem {
-  item: string
-  color: string
+  item: string;
+  color: string;
 }
 
 export interface ClothingRecommendation {
-  topWear: ClothingItem[]
-  bottomWear: ClothingItem[]
-  accessories: ClothingItem[]
-  description: string
+  topWear: ClothingItem[];
+  bottomWear: ClothingItem[];
+  accessories: ClothingItem[];
+  description: string;
 }
